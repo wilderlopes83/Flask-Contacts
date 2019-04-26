eb6cd53644e238244fa5f8ad287acb4e5dfeee0b
Wilder Lopes <wilderlopes83@gmail.com>
2019-04-25 21:37:18 -0300

    servicos de contatos integrado com BD

--- app/resources/contacts.py
+++ app/resources/contacts.py
@@ -1,14 +1,57 @@
-from flask_restful import Resource
+from flask_restful import Resource, marshal
+from app.models import Contact
+from app import request, db
+from app.schemas import contact_field
 
 class Contacts(Resource):
     def get(self):
-        return {"message": "Hello World"}
+        contacts = Contact.query.all()
+        return marshal(contacts, contact_field, "contacts")
 
     def post(self):
-        pass
+        payload = request.only(["name", "cellphone"])
+
+        name = payload["name"]
+        cellphone = payload["cellphone"]
+
+        contact = Contact(name, cellphone)
+
+        db.session.add(contact)
+        db.session.commit()
+
+        return marshal(contact, contact_field, "contact")
 
     def put(self):
-        pass
+        payload = request.only(["id", "name", "cellphone"])
+        _id = payload["id"]
+        name = payload["name"]
+        cellphone = payload["cellphone"]
+
+        contact = Contact.query.get(_id)
+        
+        if not contact:
+            return {"message": "Contato não existe!"}
+
+        contact.name = name
+
+        if cellphone:
+            contact.cellphone = cellphone
+
+        db.session.add(contact)    
+        db.session.commit()
+
+        return marshal(contact, contact_field, "contact")
 
     def delete(self):
-        pass
+        payload = request.only(["id"])
+        _id = payload["id"]
+
+        contact = Contact.query.get(_id)
+
+        if not contact:
+            return {"message": "Contato não existe!"}
+        
+        db.session.delete(contact)
+        db.session.commit()
+
+        return marshal(contact, contact_field, "contact")
